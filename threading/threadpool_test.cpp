4f7838cd6ba48e3cba0f09fba7688099bc2a905a
benbearchen <benbearchen@gmail.com>
2015-10-16 03:23:22 +0800

    More thread pool test

--- threading/threadpool_test.cpp
+++ threading/threadpool_test.cpp
@@ -16,10 +16,22 @@ int main() {
 			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 			});
 	pool.post_dealyed(520, [](){std::cout << tick() << " delay 520ms" << std::endl;});
-	pool.post_dealyed(10, [](){std::cout << tick() << " delay 10ms" << std::endl;});
-	pool.post_dealyed(149, [](){std::cout << tick() << " delay 149ms" << std::endl;});
+	pool.post_dealyed(10, [&](){
+			std::cout << tick() << " delay 10ms" << std::endl;
+			pool.post_dealyed(50, [](){std::cout << tick() << " delay 50ms on delay 10ms" << std::endl;});
+			});
+	pool.post_dealyed(149, [&](){
+			std::cout << tick() << " delay 149ms" << std::endl;
+			pool.post([](){std::cout << tick() << " again on delay 149ms" << std::endl;});
+			});
 
 	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	boot = std::chrono::steady_clock::now();
+	pool.post_dealyed(0, [](){std::cout << tick() << " delay 0ms" << std::endl; });
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
 	pool.stop();
 	pool.join();
 }
