aab142264f580ce0ba07673482babdd9a0e04dfb
benbearchen <benbearchen@gmail.com>
2015-10-22 22:06:48 +0800

    Split thread pool and timer

--- threading/threadpool.hpp
+++ threading/threadpool.hpp
@@ -1,7 +1,6 @@
 #ifndef __COMMON_THREAD_POOL_HPP__
 #define __COMMON_THREAD_POOL_HPP__
 
-#include <algorithm>
 #include <list>
 #include <thread>
 
@@ -9,14 +8,10 @@
 
 namespace common {
 
+template <typename Task = std::function<void()>>
 class ThreadPool {
 public:
-	typedef std::function<void()> Task;
-
 	ThreadPool() : work_doze(1000) {
-		auto t = std::thread([this](){thread_timer();});
-		timer_thread.swap(t);
-
 		auto w = std::thread([this](){thread_work();});
 	       	work_thread.swap(w);
 	}
@@ -31,68 +26,29 @@ public:
 
 	void stop() {
 		stopped = true;
-		timer_doze.notify();
 		work_doze.notify();
 	}
 
 	void post(Task task) {
 		std::unique_lock<std::mutex> u(mutex);
-		tasks.push_back(task);
-		work_doze.notify();
-	}
+        if (tasks.empty()) {
+            work_doze.notify();
+        }
 
-	void post_dealyed(int ms, Task task) {
-		std::unique_lock<std::mutex> u(mutex);
-		auto until = std::chrono::steady_clock::now() + std::chrono::milliseconds(ms);
-		auto f = [=](const DelayTask& dt) { return dt.until > until; };
-		auto i = std::find_if(delay_tasks.begin(), delay_tasks.end(), f);
-		delay_tasks.insert(i, DelayTask{until, task});
-		timer_doze.notify();
+		tasks.push_back(task);
 	}
 
 	void join() {
-		if (timer_thread.joinable())
-			timer_thread.join();
-
 		if (work_thread.joinable())
 			work_thread.join();
 	}
 
 private:
-	struct DelayTask {
-		std::chrono::steady_clock::time_point until;
-		Task task;
-	};
-
-	void thread_timer() {
-		while (!stopped) {
-			timer_doze.wait_until(next_time_point());
-			if (!stopped) {
-				check_delay();
-			}
-		}
-	}
-
 	void thread_work() {
 		while (!stopped) {
 			work_doze.wait();
 			check_work();
 		}
-	}
-
-	void check_delay() {
-		std::unique_lock<std::mutex> u(mutex);
-		if (delay_tasks.empty()) {
-			return;
-		}
-
-		auto now = std::chrono::steady_clock::now();
-		while (!delay_tasks.empty() && delay_tasks.front().until <= now) {
-			tasks.push_back(delay_tasks.front().task);
-			delay_tasks.pop_front();
-		}
-
-		work_doze.notify();
 	}
 
 	void check_work() {
@@ -114,23 +70,10 @@ private:
 		}
 	}
 
-	std::chrono::steady_clock::time_point next_time_point() {
-		std::unique_lock<std::mutex> u(mutex);
-		if (!delay_tasks.empty()) {
-			return delay_tasks.front().until;
-		} else {
-			return std::chrono::steady_clock::now() + std::chrono::seconds(1);
-		}
-	}
-
-	std::thread timer_thread;
-	Doze timer_doze;
-
 	std::thread work_thread;
 	Doze work_doze;
 
 	std::mutex mutex;
-	std::list<DelayTask> delay_tasks;
 	std::list<Task> tasks;
 
 	bool stopped = false;
--- threading/threadpool_test.cpp
+++ threading/threadpool_test.cpp
@@ -1,4 +1,5 @@
 #include "threadpool.hpp"
+#include "threadpool_timer.hpp"
 
 #include <functional>
 #include <iostream>
@@ -6,7 +7,8 @@
 auto boot = std::chrono::steady_clock::now();
 
 double tick() {
-	return std::chrono::duration<double>(std::chrono::steady_clock::now() - boot).count();
+    auto d = std::chrono::steady_clock::now() - boot;
+	return std::chrono::duration<double>(d).count();
 }
 
 common::Doze doze;
@@ -36,30 +38,36 @@ private:
 };
 
 int main() {
-	common::ThreadPool pool;
+	common::ThreadPool<> pool;
+    common::ThreadPoolTimer<> timer([&](std::function<void()> task){pool.post(task);});
 
-	pool.post([](){
-			std::cout << tick() << " right now" << std::endl;
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
-			});
-	pool.post_dealyed(520, [](){std::cout << tick() << " delay 520ms" << std::endl;});
-	pool.post_dealyed(10, [&](){
-			std::cout << tick() << " delay 10ms" << std::endl;
-			pool.post_dealyed(50, [](){std::cout << tick() << " delay 50ms on delay 10ms" << std::endl;});
-			});
-	pool.post_dealyed(149, [&](){
-			std::cout << tick() << " delay 149ms" << std::endl;
-			pool.post([](){std::cout << tick() << " again on delay 149ms" << std::endl;});
-			});
+    pool.post([](){
+              std::cout << tick() << " right now" << std::endl;
+              std::this_thread::sleep_for(std::chrono::milliseconds(50));
+              });
+    timer.post(520, [](){std::cout << tick() << " delay 520ms" << std::endl;});
+    timer.post(10, [&](){
+               std::cout << tick() << " delay 10ms" << std::endl;
+               timer.post(50, [](){
+                          std::cout << tick() << " delay 50ms on delay 10ms"
+                          << std::endl;
+                          });
+               });
+    timer.post(149, [&](){
+               std::cout << tick() << " delay 149ms" << std::endl;
+               pool.post([](){
+                         std::cout << tick() << " again on delay 149ms" << std::endl;
+                         });
+               });
 
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 
 	std::cout << std::endl;
 	boot = std::chrono::steady_clock::now();
-	pool.post_dealyed(0, [](){std::cout << tick() << " first delay 0ms" << std::endl; });
-	pool.post_dealyed(0, [](){std::cout << tick() << " second delay 0ms" << std::endl; });
-	pool.post([](){std::cout << tick() << " first no delay" << std::endl; });
-	pool.post([](){std::cout << tick() << " second no delay" << std::endl; });
+    timer.post(0, [](){ std::cout << tick() << " first delay 0ms" << std::endl; });
+    timer.post(0, [](){ std::cout << tick() << " second delay 0ms" << std::endl; });
+	pool.post([](){ std::cout << tick() << " first no delay" << std::endl; });
+	pool.post([](){ std::cout << tick() << " second no delay" << std::endl; });
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
@@ -67,15 +75,17 @@ int main() {
 		std::cout << std::endl;
 		boot = std::chrono::steady_clock::now();
 		int c = 10000;
-		postself test_post_self([&](std::function<void()> p){pool.post(p);}, c);
+		postself test_pool([&](std::function<void()> p){pool.post(p);}, c);
 		doze.wait_until(boot+std::chrono::minutes(1));
 		double t = tick(); 
-		std::cout << t << "s/" << c << " per " << (t / c * 1000000) << "us, post self" << std::endl;
+		std::cout << t << "s/" << c << " per " << (t / c * 1000000)
+            << "us, post self" << std::endl;
 
 		boot = std::chrono::steady_clock::now();
-		postself test_post_delay_self([&](std::function<void()> p){pool.post_dealyed(0, p);}, c);
+		postself test_timer([&](std::function<void()> p){timer.post(0, p);}, c);
 		doze.wait_until(boot+std::chrono::minutes(1));
 		t = tick(); 
-		std::cout << t << "s/" << c << " per " << (t / c * 1000000) << "us, post delay self" << std::endl;
+		std::cout << t << "s/" << c << " per " << (t / c * 1000000)
+            << "us, post delay self" << std::endl;
 	}
 }
