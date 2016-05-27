d6f51c63a6eb54258bc45d55818228dbab172fbe
benbearchen <benbearchen@gmail.com>
2016-05-27 15:46:24 +0800

    parse std::array<uint8_t, 16>

--- array/array16.cpp
+++ array/array16.cpp
@@ -28,6 +28,35 @@ struct MD5: public std::array<uint8_t, 16> {
 
         return std::move(std::string(buf));
     }
+
+    static bool parse(const std::string& hex, MD5* md5) {
+        bool succ = true;
+        size_t s = hex.size() / 2;
+        if (s > md5->size()) {
+            s = md5->size();
+        }
+
+        const char* p = hex.c_str();
+        uint8_t* d = md5->data();
+        for (int i = 0; i < s; ++i) {
+            unsigned int b = 0;
+            if (sscanf(p, "%02x", &b) != 1) {
+                succ = false;
+            }
+
+            d[i] = b;
+            p += 2;
+        }
+
+        if (s < md5->size()) {
+            succ = false;
+            for (int i = s; i < md5->size(); ++i) {
+                d[i] = 0;
+            }
+        }
+
+        return succ;
+    }
 };
 
 int main() {
@@ -47,4 +76,21 @@ int main() {
     std::array<uint8_t, 16> a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     md5 = a;
     std::cout << md5.hex() << "/" << md5.size() << " sum:" << md5.sum() << std::endl;
+
+    MD5 ooo;
+    if (!MD5::parse(md5.hex(), &ooo)) {
+        std::cout << "test failed: parse() return false" << std::endl;
+    } else {
+        if (ooo != md5) {
+            std::cout << "test failed: MD5.parse() parse(" << md5.hex() << ") out: " << ooo.hex() << std::endl;
+        }
+    }
+
+    if (MD5::parse("x01 452", &ooo)) {
+        std::cout << "test failed: parse(invalid hex string) return true" << std::endl;
+    }
+
+    if (ooo.hex() != "00014500000000000000000000000000") {
+        std::cout << "test failed: parse(invalid hex string) return " << ooo.hex() << std::endl;
+    }
 }
