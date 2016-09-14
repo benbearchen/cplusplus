acead114d2cdc24453ac687bd1264edea52e17b1
benbearchen <benbearchen@gmail.com>
2016-09-14 21:47:03 +0800

    Combine threadpool & threadpool_timer
    To make sure that at most ONE overwrited task is waiting for running.

--- threading/threadpool.hpp
+++ threading/threadpool.hpp
@@ -1,7 +1,9 @@
 #ifndef __COMMON_THREAD_POOL_HPP__
 #define __COMMON_THREAD_POOL_HPP__
 
+#include <algorithm>
 #include <list>
+#include <map>
 #include <thread>
 #include <vector>
 
@@ -20,6 +22,9 @@ public:
         max_num = num;
         work_threads.reserve(max_num);
         new_thread();
+
+        auto t = std::thread([this](){thread_timer();});
+        timer_thread.swap(t);
     }
 
     ~ThreadPool() {
@@ -33,6 +38,7 @@ public:
     void stop() {
         stopped = true;
         work_doze.notify();
+        timer_doze.notify();
     }
 
     void post(Task task) {
@@ -46,7 +52,60 @@ public:
         tasks.push_back(task);
     }
 
+    void post(int ms, Task task) {
+        std::unique_lock<std::mutex> u(mutex);
+        auto until = std::chrono::steady_clock::now() + std::chrono::milliseconds(ms);
+        auto f = [=](const DelayTask& dt) { return dt.until > until; };
+        auto i = std::find_if(delay_tasks.begin(), delay_tasks.end(), f);
+        delay_tasks.insert(i, DelayTask{until, task});
+        timer_doze.notify();
+    }
+
+    typedef int EVENT;
+
+    EVENT event_id() {
+        std::unique_lock<std::mutex> u(mutex);
+        return next_event_id++;
+    }
+
+    void post_overwrite(EVENT e, int ms, Task task) {
+        auto until = std::chrono::steady_clock::now() + std::chrono::milliseconds(ms);
+        post_overwrite(e, until, task);
+    }
+
+    void post_overwrite(EVENT e, std::chrono::steady_clock::time_point until, Task task) {
+        std::unique_lock<std::mutex> u(mutex);
+
+        if (arrival_event_tasks.find(e) != arrival_event_tasks.end()) {
+            return;
+        }
+
+        auto p = event_tasks.find(e);
+        if (p == event_tasks.end()) {
+            event_tasks[e] = DelayTask{until, task};
+        } else {
+            if (p->second.until > until) {
+                p->second = DelayTask{until, task};
+            } else {
+                return;
+            }
+        }
+
+        timer_doze.notify();
+    }
+
+    void remove(EVENT e) {
+        std::unique_lock<std::mutex> u(mutex);
+        arrival_event_tasks.erase(e);
+        event_tasks.erase(e);
+    }
+
 private:
+    struct DelayTask {
+        std::chrono::steady_clock::time_point until;
+        Task task;
+    };
+
     void join() {
         for (auto& work_thread : work_threads) {
             if (work_thread.joinable())
@@ -54,12 +113,24 @@ private:
         }
 
         work_threads.clear();
+
+        if (timer_thread.joinable()) {
+            timer_thread.join();
+        }
     }
 
     void thread_work() {
         while (!stopped) {
             work_doze.wait();
             check_work();
+            check_delay();
+        }
+    }
+
+    void thread_timer() {
+        while (!stopped) {
+            auto t = flush_delay();
+            timer_doze.wait_until(t);
         }
     }
 
@@ -85,6 +156,30 @@ private:
         }
     }
 
+    void check_delay() {
+        while (!stopped) {
+            Task task;
+            {
+                std::unique_lock<std::mutex> u(mutex);
+                if (arrival_event_tasks.empty()) {
+                    return;
+                }
+
+                auto p = arrival_event_tasks.begin();
+                task = p->second.task;
+                arrival_event_tasks.erase(p);
+
+                if (!arrival_event_tasks.empty()) {
+                    work_doze.notify();
+                }
+            }
+
+            if (task) {
+                task();
+            }
+        }
+    }
+
     void new_thread() {
         int c = int(work_threads.size());
         if (c < max_num) {
@@ -94,14 +189,73 @@ private:
         }
     }
 
+    std::chrono::steady_clock::time_point flush_delay() {
+        std::unique_lock<std::mutex> u(mutex);
+        std::chrono::steady_clock::time_point zero;
+        std::chrono::steady_clock::time_point t;
+
+        bool arrival = false;
+        auto now = std::chrono::steady_clock::now();
+
+        for (auto i = std::begin(event_tasks); i != std::end(event_tasks); ) {
+            auto et = i->second.until;
+            if (et <= now) {
+                arrival = true;
+                arrival_event_tasks.insert(*i);
+                event_tasks.erase(i++);
+                continue;
+            }
+
+            ++i;
+            if (t == zero || t > et) {
+                t = et;
+            }
+        }
+
+        while (!delay_tasks.empty()) {
+            auto dt = delay_tasks.front().until;
+            if (dt <= now) {
+                arrival = true;
+                tasks.push_back(delay_tasks.front().task);
+                delay_tasks.pop_front();
+                continue;
+            }
+
+            if (t == zero || t > dt) {
+                t = dt;
+            }
+
+            break;
+        }
+
+        if (t == zero) {
+            t = now + std::chrono::seconds(1);
+        }
+
+        if (arrival) {
+            work_doze.notify();
+        }
+
+        return t;
+    }
+
+    int max_num = 1;
+    int volatile next_event_id = 1;
+
     std::vector<std::thread> work_threads;
     Doze work_doze;
 
-    std::mutex mutex;
-    std::list<Task> tasks;
+    std::thread timer_thread;
+    Doze timer_doze;
 
-    int max_num = 1;
-    bool stopped = false;
+    std::mutex mutex;
+
+    std::list<Task> tasks;
+    std::list<DelayTask> delay_tasks;
+    std::map<EVENT, DelayTask> arrival_event_tasks;
+    std::map<EVENT, DelayTask> event_tasks;
+
+    bool volatile stopped = false;
 };
 
 }
--- threading/threadpool_test.cpp
+++ threading/threadpool_test.cpp
@@ -1,5 +1,4 @@
 #include "threadpool.hpp"
-#include "threadpool_timer.hpp"
 
 #include <functional>
 #include <iostream>
@@ -64,37 +63,51 @@ void testmulti() {
     pool.post(std::bind(test_pool, 4));
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    boot = std::chrono::steady_clock::now();
+    std::cout << std::endl;
+
+    int zerot = 0;
+    int zerotimes = 10000;
+    auto eid = pool.event_id();
+    for (int i = 0; i < zerotimes; i++) {
+        pool.post_overwrite(eid, 0, [&]() { zerot++; pool.post([]() { std::this_thread::sleep_for(std::chrono::milliseconds(1)); }); });
+    }
+
+    std::cout << tick() << " post zero finished" << std::endl;
+    pool.post([&]() { std::cout << tick() << " post zero " << zerotimes << " times, got " << zerot << std::endl; });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
 int main() {
     common::ThreadPool<> pool;
-    common::ThreadPoolTimer<> timer([&](std::function<void()> task){pool.post(task);});
 
     pool.post([](){
               std::cout << tick() << " right now" << std::endl;
               std::this_thread::sleep_for(std::chrono::milliseconds(50));
               });
-    timer.post(520, [](){std::cout << tick() << " delay 520ms" << std::endl;});
-    timer.post(10, [&](){
-               std::cout << tick() << " delay 10ms" << std::endl;
-               timer.post(50, [](){
-                          std::cout << tick() << " delay 50ms on delay 10ms"
-                          << std::endl;
-                          });
-               });
-    timer.post(149, [&](){
-               std::cout << tick() << " delay 149ms" << std::endl;
-               pool.post([](){
-                         std::cout << tick() << " again on delay 149ms" << std::endl;
-                         });
-               });
+    pool.post(520, [](){std::cout << tick() << " delay 520ms" << std::endl;});
+    pool.post(10, [&](){
+              std::cout << tick() << " delay 10ms" << std::endl;
+              pool.post(50, [](){
+                        std::cout << tick() << " delay 50ms on delay 10ms"
+                        << std::endl;
+                        });
+              });
+    pool.post(149, [&](){
+              std::cout << tick() << " delay 149ms" << std::endl;
+              pool.post([](){
+                        std::cout << tick() << " again on delay 149ms" << std::endl;
+                        });
+              });
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     std::cout << std::endl;
     boot = std::chrono::steady_clock::now();
-    timer.post(0, [](){ std::cout << tick() << " first delay 0ms" << std::endl; });
-    timer.post(0, [](){ std::cout << tick() << " second delay 0ms" << std::endl; });
+    pool.post(0, [](){ std::cout << tick() << " first delay 0ms" << std::endl; });
+    pool.post(0, [](){ std::cout << tick() << " second delay 0ms" << std::endl; });
     pool.post([](){ std::cout << tick() << " first no delay" << std::endl; });
     pool.post([](){ std::cout << tick() << " second no delay" << std::endl; });
 
@@ -111,7 +124,7 @@ int main() {
             << "us, post self" << std::endl;
 
         boot = std::chrono::steady_clock::now();
-        postself test_timer([&](std::function<void()> p){timer.post(0, p);}, c);
+        postself test_timer([&](std::function<void()> p){pool.post(0, p);}, c);
         doze.wait_until(boot+std::chrono::minutes(1));
         t = tick(); 
         std::cout << t << "s/" << c << " per " << (t / c * 1000000)
@@ -119,18 +132,34 @@ int main() {
     }
 
     boot = std::chrono::steady_clock::now();
+    std::cout << std::endl;
 
-    auto eid = timer.event_id();
-    timer.post_overwrite(eid, 1000, [&]() { std::cout << tick() << " event 1s" << std::endl; });
-    timer.post_overwrite(eid, 100, [&]() { std::cout << tick() << " event 0.1s" << std::endl; });
-    timer.post_overwrite(eid, 2000, [&]() { std::cout << tick() << " event 0.2s" << std::endl; });
+    auto eid = pool.event_id();
+    pool.post_overwrite(eid, 1000, [&]() { std::cout << tick() << " event 1s" << std::endl; });
+    pool.post_overwrite(eid, 100, [&]() { std::cout << tick() << " event 0.1s" << std::endl; });
+    pool.post_overwrite(eid, 2000, [&]() { std::cout << tick() << " event 0.2s" << std::endl; });
 
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
-    timer.post_overwrite(eid, 100, [&]() { std::cout << tick() << " should miss event 0.1s" << std::endl; });
-    timer.remove(eid);
+    pool.post_overwrite(eid, 100, [&]() { std::cout << tick() << " should miss event 0.1s" << std::endl; });
+    pool.remove(eid);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    boot = std::chrono::steady_clock::now();
+    std::cout << std::endl;
+
+    int zerot = 0;
+    int zerotimes = 10000;
+    eid = pool.event_id();
+    for (int i = 0; i < zerotimes; i++) {
+        pool.post_overwrite(eid, 0, [&]() { zerot++; std::this_thread::sleep_for(std::chrono::milliseconds(1)); });
+    }
+
+    std::cout << tick() << " post zero finished" << std::endl;
+    pool.post([&]() { std::cout << tick() << " post zero " << zerotimes << " times, got " << zerot << std::endl; });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     testmulti();
 }
